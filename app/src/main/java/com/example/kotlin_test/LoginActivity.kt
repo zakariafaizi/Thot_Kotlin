6f43d53d67c6cc7c7c155f66bd1fcbfc2879cea7
Zakaria Faizi <faizi.channel@gmail.com>
2021-01-31 12:35:30 -0500

    Initial commit

---
+++ app/src/main/java/com/example/kotlin_test/LoginActivity.kt
@@ -0,0 +1,20 @@
+package com.example.kotlin_test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+        val k = "ok";
+
+        var kp = "pk";
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/kotlin_test/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.kotlin_test
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Layout
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+
+
+    private fun showRegisteration()
+    {
+
+
+
+    }
+}
